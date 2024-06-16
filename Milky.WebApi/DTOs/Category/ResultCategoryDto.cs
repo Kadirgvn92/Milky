b1717192db0b5cb35b17b388c02c02a5b2dc2b12
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-06-16 23:37:04 +0300

    Index page started to design

---
+++ Milky.WebApi/DTOs/Category/ResultCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Milky.WebApi.DTOs.Category;
+
+public class ResultCategoryDto
+{
+    public int CategoryID { get; set; }
+    public string CategoryName { get; set; }
+    public bool IsDeleted { get; set; }
+}
---
+++ Milky.WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Milky.WebApi.DTOs.Category;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Milky.WebUI.Controllers;
+public class DefaultController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public DefaultController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:44320/api/Category");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+}
