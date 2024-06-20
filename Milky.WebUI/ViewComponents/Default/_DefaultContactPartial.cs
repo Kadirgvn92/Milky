f71e2ee24fcf774daf9e87b9de717d0bcb8d1585
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-06-20 15:27:39 +0300

    Contact and About page compoleted

---
+++ Milky.WebUI/ViewComponents/Default/_DefaultContactPartial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Milky.WebUI.DTOs.Contact;
+using Milky.WebUI.DTOs.Product;
+using Newtonsoft.Json;
+
+namespace Milky.WebUI.ViewComponents.Default;
+
+public class _DefaultContactPartial : ViewComponent
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _DefaultContactPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:44320/api/Contact");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+}
