620258b2ee5ec2543ff9223d3f461dacddb2126b
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-06-21 10:15:13 +0300

    Statistics controller and page designed and tested

---
+++ Milky.WebUI/Areas/Admin/Controllers/StatisticController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Milky.WebUI.DTOs.Statistic;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Milky.WebUI.Areas.Admin.Controllers;
+[Area("Admin")]
+[Route("Admin/[controller]/[action]/{id?}")]
+public class StatisticController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public StatisticController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    public string GenerateName()
+    {
+        string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        Random random = new Random();
+        char[] stringChars = new char[6];
+
+        for (int i = 0; i < 6; i++)
+        {
+            stringChars[i] = chars[random.Next(chars.Length)];
+        }
+
+        return new string(stringChars);
+    }
+    public async Task<IActionResult> Index(int page = 1)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:44320/api/Statistic");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultStatisticDto>>(jsonData);
+
+            return View(values);
+        }
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Update(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:44320/api/Statistic/GetStatisticById?id=" + id);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Update(ResultStatisticDto dto)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var updatedStatisticJson = JsonConvert.SerializeObject(dto);
+        var stringContent = new StringContent(updatedStatisticJson, Encoding.UTF8, "application/json");
+
+        var updateResponse = await client.PutAsync("https://localhost:44320/api/Statistic/", stringContent);
+
+        if (updateResponse.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+
+        return View(dto); 
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.DeleteAsync("https://localhost:44320/api/Statistic?id=" + id);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+}
